26808c79030703b81baf41755ea0f9a102206d6a
JshengZ <jshengz@users.noreply.github.com>
2026-07-05 15:33:29 +0800

    Rebuild analog film simulation engine

--- src/components/homepage/FilmGateWeaveLayer.tsx
+++
@@ -1,3 +0,0 @@
-export function FilmGateWeaveLayer() {
-  return null;
-}
--- src/components/homepage/FilmLightLeakBurst.tsx
+++
@@ -1,9 +0,0 @@
-type FilmLightLeakBurstProps = {
-  triggerKey: number;
-};
-
-export function FilmLightLeakBurst({ triggerKey }: FilmLightLeakBurstProps) {
-  void triggerKey;
-
-  return null;
-}
